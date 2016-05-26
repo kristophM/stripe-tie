b9f42c189c58447303b68fcf12f15e97ce5c8e8a
kristophM <krist.m@gmail.com>
2016-05-26 18:22:44 -0400

    placeholder tests for account and customer

---
+++ spec/stripetie/models/account_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Account do
+  #Write tests for helper methods in Company model (use Rolify as an example)
+  #Should have attribute for stripe_account_id
+end
---
+++ spec/stripetie/models/customer_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Customer do
+  #Write tests for helper methods in Customer model (use Rolify as an example)
+  #Should have attribute for stripe_customer_id
+end
